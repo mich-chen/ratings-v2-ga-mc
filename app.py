e2311636890c4815846eb33e3dca9fbf3f7df762
Michelle Chen <mich.chen.94@gmail.com>
2021-04-25 00:27:42 -0700

    set up homepage and catch all route

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ Running Flask options:
         - (env) $ export FLASK_ENV=development
         - (env) $ export FLASK_DEBUG=1     # 1 to enable, 0 to disable
         - (env) $ export FLASK_RUN_PORT=5000
-        - (env) $ flask run --port --debug --env
+        - (env) $ flask run --port --debug --env --host
         - setting env variables specify flask how to load the app
         - can maintain code independent of external env configs
 
@@ -32,3 +32,22 @@ Running Flask options:
 
 Author: Michelle Chen
 """
+
+from flask import Flask, render_template, request, session
+
+# set up flask app
+app = Flask(__name__)
+app.secret_key = 'dev'
+
+# catch-all route
+@app.route('/', defaults={'path': ''})
+@app.route('/<string:path>') # specifying anything string in path
+@app.route('/<path:path>')
+def catch_all(path):
+    """Catch all URL routes that don't match specific path."""
+    return render_template('homepage.html')
+
+@app.route('/')
+def homepage():
+    """Render homepage."""
+    return render_template('homepage.html')
