606737bb9e0137d9b828b74d1b8921b4c0b95b2f
Michelle Chen <mich.chen.94@gmail.com>
2021-04-24 23:09:16 -0700

    new app file for practicing with Flask

---
+++ app.py
@@ -0,0 +1,12 @@
+"""
+Practice Flask app with SQLAlchemy and postgres DB.
+
+Requirements:
+    - Python 3.*
+    - PostgreSQL
+    - Flask
+    - SQLAlchemy
+    - Pytest
+
+Author: Michelle Chen
+"""
